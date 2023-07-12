ddcff9ce3fc16c9ea652d9a773df402c14565fd7
Guillermo Ronie Salcedo Alvarez <139187191+guisa17@users.noreply.github.com>
2023-07-12 15:49:03 -0500

    Update bfs.cpp

--- BFS/bfs.cpp
+++ BFS/bfs.cpp
@@ -102,6 +102,7 @@ void grafo::insertarAristas(int x, int y)
 {
     insertarArista(x, y);
     insertarArista(y, x);
+    numAristas++;
 }
 
 
@@ -117,7 +118,6 @@ void grafo::insertarArista(int x, int y)
         a->datoDestino = y;
         a->sgtArista = p->adyacente;
         p->adyacente = a;
-        numAristas++;
     } 
 }
 
